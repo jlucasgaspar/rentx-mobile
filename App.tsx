e2098e6c74df5d42dca84b9394752e8cf72cf094
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-08-20 16:52:19 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
+import { Home } from './src/screens/Home';
+import { CarDetails } from './src/screens/CarDetails';
 
-export default function App() {
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_500Medium,
+    Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold
+  });
+
+  if (!fontsLoaded) return <AppLoading />
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      {/* <Home /> */}
+      <CarDetails />
+    </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,81 @@
+import React, {useEffect, useState} from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
+// import { useTheme } from 'styled-components'; 
+// import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+// import { Accessory } from '../../components/Accessory';
+// import { Button } from '../../components/Button';
+
+// import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+// import { CarDTO } from '../../dtos/CarDTO';
+// import { Car as CarModel} from '../../database/model/Car';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Content,
+  Accessories,
+  Footer,
+  OfflineInfo
+} from './styles';
+
+// interface Params {
+//   car: CarModel;
+// }
+
+export function CarDetails(){
+  return (
+    <Container>
+      <StatusBar 
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"      
+      />
+
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider imagesUrl={[{ id: '1', photo: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png' }]} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Ferrari</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Lorem ipsum qualquer
+        </About>
+      </Content>
+    </Container>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    position: 'absolute',
+    overflow: 'hidden', 
+    zIndex: 1,
+  },
+})
